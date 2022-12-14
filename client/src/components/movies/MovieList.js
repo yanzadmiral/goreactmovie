cdfc6cd56ee4744aa944ee06bcab77a31e24a97d
Yayan Cahyani <yayancloud@gmail.com>
2022-12-14 22:56:44 +0700

    17,18,19,20

--- client/src/components/movies/MovieList.js
+++ client/src/components/movies/MovieList.js
@@ -1,35 +1,64 @@
 import React, { useState,useEffect} from 'react'
 import { Link } from 'react-router-dom';
+import axios from 'axios'
 
 const MovieList = () => {
 
   const [movies, setMovies] = useState([]);
+  const [loaded, setLoaded] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null);
+
   useEffect(() => {
-    setMovies([
-      { id: 1, title: "asdasd", runtime: 145 },
-      { id: 2, title: "12", runtime: 145 },
-      { id: 3, title: "asdasadds", runtime: 145 },
-    ]);
+    const fetchMovie = async () => {
+     try {
+       const result = await axios(`http://localhost:4000/movies`);
+       console.log(result)
+       await setMovies(result.data.movies);
+       setLoaded(true);
+     } catch (error) {
+       setErrorMessage(error.response.data);
+     }
+    };
+    fetchMovie();
   }, [])
   
 
   return (
-    <div className="row">
-      {movies.map((movie, index) => ( 
-        <div className="col-sm-4 mb-2" key={index}>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <Link to={`/movies/${movie.id}`} className="btn btn-primary">Detail</Link>
+    <>
+      {!loaded ? (
+        (() => {
+          if (errorMessage) {
+            return (
+              <div className="row">
+                <p>Opps....{errorMessage}</p>
+              </div>
+            );
+          } else {
+            return (
+              <div className="row">
+                <p>Loading</p>
+              </div>
+            );
+          }
+        })()
+      ) : (
+        <div className="row">
+          {movies.map((movie, index) => (
+            <div className="col-sm-4 mb-2" key={index}>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{movie.title}</h5>
+                  <p className="card-text">{movie.description}</p>
+                  <Link to={`/movies/${movie.id}`} className="btn btn-primary">
+                    Detail
+                  </Link>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 }
 
--- client/src/pages/Movies/Show.js
+++ client/src/pages/Movies/Show.js
@@ -1,31 +1,86 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link ,useParams } from 'react-router-dom';
+import axios from "axios";
 
 const Show = () => {
-    let { id } = useParams();
+  let { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+   useEffect(() => {
+    const fetchMovie = async () => {
+     try {
+       const result = await axios(`http://localhost:4000/movie/${id}`);
+       await setMovie(result.data.movie);
+       setLoaded(true);
+     } catch (error) {
+       setErrorMessage(error.response.data);
+     }
+    };
+    fetchMovie();
+  }, [])
+
   return (
     <>
-      <h2>Movie: {id}</h2>
-      <div className="float-start">
-        <small>Rating: </small>
-      </div>
-      <div className="float-end">
-        <span className="badge bg-secondary me-1">Action</span>
-      </div>
-      <div className="clearfix"></div>
-      <hr />
-      <div className="table-responsive">
-        <table className="table table-striped table-dark">
-          <tbody>
-            <tr>
-              <td>title:</td>
-            </tr>
-            <tr>
-              <td>Description</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      {!loaded ? (
+        (() => {
+          if (errorMessage) {
+            return (
+              <div className="row">
+                <p>Opps....{errorMessage}</p>
+              </div>
+            );
+          } else {
+            return (
+              <div className="row">
+                <p>Loading</p>
+              </div>
+            );
+          }
+        })()
+      ) : (
+        <>
+          <pre>{JSON.stringify(movie)}</pre>
+          <h2>
+            Movie: {movie.title} / ({movie.year})
+          </h2>
+          <div className="float-start">
+            <small>Rating: {movie.mpaa_rating}</small>
+          </div>
+          <div className="float-end">
+            {Object.entries(movie.genre).map((genre, index) => (
+              <Link
+                className="badge bg-secondary me-1"
+                to={`/genres/${genre[0]}`}
+                key={index}
+              >
+                {genre[1]}
+              </Link>
+            ))}
+          </div>
+          <div className="clearfix"></div>
+          <hr />
+          <div className="table-responsive">
+            <table className="table table-striped table-dark">
+              <tbody>
+                <tr>
+                  <td>title:</td>
+                  <td>{movie.title}</td>
+                </tr>
+                <tr>
+                  <td>{movie.description}</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Runtime:</td>
+                  <td>{movie.runtime} Minute(s)</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </>
+      )}
     </>
   );
 }
